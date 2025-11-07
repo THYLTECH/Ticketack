4c407b7e954cc2f99721b08b8972946e7f7b6821
Hakim <hakimfidjel.pro@gmail.com>
2025-11-07 11:10:40 +0100

    [FEAT] - Migrations

---
+++ database/migrations/2025_11_07_095814_create_notifications_table.php
@@ -0,0 +1,48 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('notification_preferences', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained()->cascadeOnDelete();
+            $table->string('type'); // ex: 'ticket_created', 'ticket_assigned', 'account_created'
+            $table->enum('channel', ['email', 'sms', 'database'])->default('database');
+            $table->boolean('enabled')->default(true);
+            $table->timestamps();
+
+            $table->unique(['user_id', 'type', 'channel']);
+        });
+
+        Schema::create('notifications', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained()->cascadeOnDelete();
+            // Duplication useful for filtering and querying
+            $table->string('type'); // ex: 'ticket_created', 'ticket_assigned', 'account_created'
+            
+            $table->string('title');
+            $table->text('message')->nullable();
+            $table->string('action_url')->nullable();
+            
+            $table->datetime('read_at')->nullable();
+            $table->timestamps();
+        });   
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('notifications');
+        Schema::dropIfExists('notification_preferences');
+    }
+};
