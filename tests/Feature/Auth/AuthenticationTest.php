0aadec17da8c6379910cdb8f13b2abd625470919
Hakim <hakimfidjel.pro@gmail.com>
2025-11-07 10:25:06 +0100

    [FIX] - Authentication test

--- tests/Feature/Auth/AuthenticationTest.php
+++ tests/Feature/Auth/AuthenticationTest.php
@@ -46,7 +46,8 @@ test('users can logout', function () {
 test('users are rate limited', function () {
     $user = User::factory()->create();
 
-    RateLimiter::increment(md5('login'.implode('|', [$user->email, '127.0.0.1'])), amount: 5);
+    $key = hash('sha256', 'login' . implode('|', [$user->email, '127.0.0.1']));
+    RateLimiter::increment($key, amount: 5);
 
     $response = $this->post(route('auth.login.store'), [
         'email' => $user->email,
