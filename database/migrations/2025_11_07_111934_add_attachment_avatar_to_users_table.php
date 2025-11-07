384226e4820ad02f6b7ce6aa0b0e7ee4ad1cbace
Yns1000 <younes.bgrt@icloud.com>
2025-11-07 12:23:51 +0100

    [U] - Add attachment_avatar column to users table

---
+++ database/migrations/2025_11_07_111934_add_attachment_avatar_to_users_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->unsignedBigInteger('attachment_avatar')->nullable()->after('email');
+
+            $table->foreign('attachment_avatar')
+                ->references('id')->
+                on('attachments')->
+                onDelete('set null');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropForeign(['attachment_avatar']);
+            $table->dropColumn('attachment_avatar');
+        });
+    }
+};
