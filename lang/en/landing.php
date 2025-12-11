6858e3c600669aa7672a9c09477ca7838af62f8c
theoc <theo.colin@ig2i.centralelille.fr>
2025-12-11 18:40:18 +0100

    [FIX] - Text correction ==> english ==> french

--- lang/en/landing.php
+++ lang/en/landing.php
@@ -32,10 +32,10 @@ return [
             'companyTitle' =>'Imagined by ID Ingenierie',
             'companyDescription' =>'Specialist in customized software development and web and mobile technologies.',
             'teamTitle' => 'Developed by Thyltech',
-            'teamDescription' =>'Team of french student from Centrale Lille IG2I.'
+            'teamDescription' =>'Team of French students from Centrale Lille IG2I.'
         ],
         'achievementsTitle' => "Ticketack's Features",
-        'achievementsDescription' => 'Ticketack allow you to centralize and simplify the tracking of all your technical issues. This tool reduces difficult tracking, loss of information and lack of visibility caused by problem reports via emails.',
+        'achievementsDescription' => 'Ticketack allows you to centralize and simplify the tracking of all your technical issues. This tool reduces difficult tracking, loss of information and lack of visibility caused by problem reports via emails.',
         'achievements' => [
             '1' => [
                 'label' => 'Ticket Management',
